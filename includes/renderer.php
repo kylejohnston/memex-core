9c59f82ded4679124f1b01d2ad684a96c7445b24
Kyle Johnston <flow14@gmail.com>
2018-07-25 18:31:27 -0500

    Revised lightbox to point to full-size images, which are lazyloaded on click or tap

--- includes/renderer.php
+++ includes/renderer.php
@@ -11,9 +11,11 @@
       if ( ($file !== '.') && ($file !== '..') && (in_array( $file_type, $file_display)) ) {
         echo '
         <div class="item">
-          <a href="#',$file,'"><img class="lazyload" src="img/loader.svg" data-src="', $www_root, '/', $file, '" alt="', $file, '" alt="', $file, '" width="auto" height="auto"></a>
+          <a href="#',$file,'">
+            <img class="lazyload" src="img/loader.svg" data-src="', $www_root, '/', $file,'" alt="', $file, '" width="auto">
+          </a>
         </div>
-        <a href="#_" class="lightbox" id="',$file,'"><img src="', $www_root, '/', $file, '"></a>'
+        <a href="#_" class="lightbox" id="',$file,'"><img class="lazyload" src="img/loader.svg" data-src="', $www_root, '/fullsize/', $file, '"></a>'
         ;
         //break;
       }
