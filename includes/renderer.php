4a2d96a0425aea947cfbaad42e69ade37f93ed1f
Kyle Johnston <flow14@gmail.com>
2019-11-28 00:50:11 -0600

    Added lazyload class to fullsize image

--- includes/renderer.php
+++ includes/renderer.php
@@ -17,7 +17,7 @@
             <img class="lazyload" src="img/loader.svg" data-src="', $www_root, '/', $file,'" alt="', $file, '" width="auto">
           </a>
         </div>
-        <a href="#_" class="lightbox" id="',$file,'"><img class="lazyload" src="img/loader.svg" data-src="', $www_root, '/_fullsize/', $file, '"></a>'
+        <a href="#_" class="lightbox lazyload" id="',$file,'"><img class="lazyload" src="img/loader.svg" data-src="', $www_root, '/_fullsize/', $file, '"></a>'
         ;
         //break;
       }
